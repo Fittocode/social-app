6cd1ec91200593eb0e561149c0a7feac737eef15
Matt Harrison <MattHarrison92@gmail.com>
2022-02-04 16:26:12 -0600

    create errors view folder

--- error-handling/index.js
+++ error-handling/index.js
@@ -1,12 +1,12 @@
 module.exports = (app) => {
   app.use((req, res, next) => {
-    res.status(404).render('not-found');
+    res.status(404).render('errors/not-found');
   });
 
   app.use((err, req, res, next) => {
     console.error('ERROR', req.method, req.path, err);
     if (!res.headersSent) {
-      res.status(500).render('error');
+      res.status(500).render('errors/error');
     }
   });
 };
