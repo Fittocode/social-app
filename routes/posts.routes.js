61c2ac31cad22077b0ab7d50956f213eba617b3b
Matt Harrison <MattHarrison92@gmail.com>
2022-02-04 17:33:07 -0600

    route get update post fills in post values

--- routes/posts.routes.js
+++ routes/posts.routes.js
@@ -2,22 +2,32 @@ const router = require('express').Router();
 const Post = require('../models/Post.models');
 const User = require('../models/User.models');
 
-router.get('/:id/add-post', (req, res) => {
+router.get('/:userId/add-post', (req, res) => {
   res.render('posts/add-post', { user: req.user });
 });
 
-router.post('/:id/add-post', async (req, res) => {
-  const { id } = req.params;
+router.post('/:userId/add-post', async (req, res) => {
+  const { userId } = req.params;
   const { title, content } = req.body;
   try {
     const userPost = await Post.create({ title, author: req.user, content });
-    await User.findByIdAndUpdate(id, { $push: { posts: userPost } });
+    await User.findByIdAndUpdate(userId, { $push: { posts: userPost } });
     res.redirect('/user-profile');
   } catch (err) {
     console.log(err);
   }
 });
 
-router.get('/users/:id/post', (req, res) => {});
+router.get(`/:postId/update`, async (req, res) => {
+  const { postId } = req.params;
+  try {
+    const postDB = await Post.findById(postId);
+    console.log(postDB);
+    console.log(postDB.content);
+    res.render('posts/update-post', { user: req.user, post: postDB });
+  } catch (err) {
+    console.log(err.message);
+  }
+});
 
 module.exports = router;
