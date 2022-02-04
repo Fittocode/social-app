dd27bf8f8caa133b17089f2ae3249caa602d2b04
Matt Harrison <MattHarrison92@gmail.com>
2022-02-04 14:31:56 -0600

    passport authentication for login

--- app.js
+++ app.js
@@ -1,12 +1,77 @@
 const express = require('express');
-const connectDB = require('./db/index');
+require('dotenv/config');
 const app = express();
-
-// connect to MongoDB
-connectDB();
 
 //middleware config
 require('./config/index')(app);
+
+// passport authentication
+const session = require('express-session');
+const MongoStore = require('connect-mongo');
+const flash = require('connect-flash');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const bcrypt = require('bcryptjs');
+const User = require('./models/User.models');
+
+app.use(flash());
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { maxAge: 60000 },
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGODB_URI,
+      ttl: 60 * 60 * 24,
+    }),
+  })
+);
+
+passport.serializeUser((user, done) => done(null, user._id));
+
+passport.deserializeUser(async (id, done) => {
+  const user = await User.findById(id);
+  try {
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: 'username',
+      passwordField: 'password',
+    },
+    async (username, password, done) => {
+      const user = await User.findOne({ username });
+      try {
+        if (!user) {
+          return done(null, false, {
+            message: 'Incorrect Username',
+          });
+        }
+        if (!bcrypt.compareSync(password, user.password)) {
+          return done(null, false, {
+            message: 'Incorrect password',
+          });
+        }
+        done(null, user);
+      } catch (err) {
+        done(err);
+      }
+    }
+  )
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+// connect to MongoDB
+const connectDB = require('./db/index');
+connectDB();
 
 // Routes
 const index = require('./routes/index.routes');
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,10 +1,21 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
+const { Mongoose } = require('mongoose');
+const passport = require('passport');
 const User = require('../models/User.models');
 
 router.get('/login', (req, res) => {
   res.render('auth/login');
 });
+
+router.post(
+  '/login',
+  passport.authenticate('local', {
+    successRedirect: '/user-profile',
+    failureRedirect: '/login',
+    failureFlash: true,
+  })
+);
 
 router.get('/signup', (req, res) => {
   res.render('auth/signup');
@@ -14,12 +25,14 @@ router.post('/signup', async (req, res) => {
   const { username, password } = req.body;
 
   try {
-    const passwordHash = await bcrypt.hashSync(password, 10);
+    const passwordHash = await bcrypt.hash(password, 10);
     await User.create({ username, password: passwordHash });
     console.log('user created');
     res.render('auth/login');
   } catch (err) {
-    console.log(err);
+    res.status(500).render('auth/signup', {
+      error: 'That username is already in use',
+    });
   }
 });
 
